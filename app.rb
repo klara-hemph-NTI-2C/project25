bd966a9bb71ade26ac32338565bfd3c32add96b1
klara-hemph-NTI-2C <klara.hemph@elev.ga.ntig.se>
2025-02-04 16:19:36 +0100

    n

--- app.rb
+++ app.rb
@@ -2,4 +2,36 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+# require 'becrypt'
+enable :sessions
+
+# def connect_to_db()
+#   db = SQLite3::Database.new("db/Databas.db")
+#   db.results_as_hash = true
+
+
+get('/')  do
+  slim(:start)
+end 
+
+get('/profilsida') do
+  slim(:profil)
+end
+
+
+get('/birds') do
+  db = SQLite3::Database.new("db/Databas.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM birds")
+  p result
+  slim(:index ,locals:{birds:result})
+end
+
+get('/birds/:id') do
+  id = params[:id].to_i
+  db = SQLite3::Database.new("db/Databas.db")
+  db.results_as_hash = true
+  result = db.execute("SELECT * FROM birds WHERE Bird_id = ?",id).first
+  slim(:"birds/show",locals:{result:result})
+end
+
